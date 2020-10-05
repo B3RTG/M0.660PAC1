450dadd8467e401f42f20b0b84ddc0d1b71babf1
B3RTG <albert.kais@gmail.com>
2020-10-05 22:32:43 +0200

    Exercici 1: Revisió de la branca

--- src/app/src/main/java/edu/uoc/android/pec1library/BookDetailActivity.kt
+++ src/app/src/main/java/edu/uoc/android/pec1library/BookDetailActivity.kt
@@ -3,6 +3,7 @@ package edu.uoc.android.pec1library
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import edu.uoc.android.pec1library.fragment.BookDetailFragment
 import kotlinx.android.synthetic.main.activity_book_detail.*
 
 class BookDetailActivity : AppCompatActivity() {
@@ -19,5 +20,9 @@ class BookDetailActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        val ft = supportFragmentManager.beginTransaction()
+        val fBookFragment = BookDetailFragment()
+        ft.replace(R.id.fl_book_detail, fBookFragment )
+        ft.commit()
     }
 }
